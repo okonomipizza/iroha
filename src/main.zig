659f49f3eed70b5c9d5d76279bc4ffd8830afeb4
okonomipizza <140386510+okonomipizza@users.noreply.github.com>
2025-10-23 21:50:55 +0900

    refactor main.zig

--- src/main.zig
+++ src/main.zig
@@ -31,61 +31,57 @@ fn activate(app: *gtk.Application, user_data: ?*anyopaque) callconv(.c) void {
         gtk.StyleContext.addClass(style_context, "system-bar");
 
         // Make window style like system bar
-        layer_shell.initForWindow(window);
-        layer_shell.setLayer(window, layer_shell.GTK_LAYER_SHELL_LAYER_TOP);
-        layer_shell.setAnchor(window, layer_shell.GTK_LAYER_SHELL_EDGE_TOP, true);
-        layer_shell.setAnchor(window, layer_shell.GTK_LAYER_SHELL_EDGE_LEFT, true);
-        layer_shell.setAnchor(window, layer_shell.GTK_LAYER_SHELL_EDGE_RIGHT, true);
-        layer_shell.setExclusiveZone(window, 30);
+        layer_shell.setupLayerShell(window);
 
-        var main_box = gtk.Box.new(gtk.Orientation.horizontal, 1);
-        gtk.Widget.setHalign(main_box.as(gtk.Widget), gtk.Align.fill);
-        gtk.Widget.setValign(main_box.as(gtk.Widget), gtk.Align.center);
-        gtk.Window.setChild(window.as(gtk.Window), main_box.as(gtk.Widget));
-
-        var left_box = gtk.Box.new(gtk.Orientation.horizontal, 1);
-        gtk.Widget.setHalign(left_box.as(gtk.Widget), gtk.Align.start);
-        gtk.Widget.setValign(left_box.as(gtk.Widget), gtk.Align.center);
-
-        var right_box = gtk.Box.new(gtk.Orientation.horizontal, 1);
-        gtk.Widget.setHalign(right_box.as(gtk.Widget), gtk.Align.start);
-        gtk.Widget.setValign(right_box.as(gtk.Widget), gtk.Align.center);
-
-        // Create clock widget (JST)
-        var clock = Clock.new(9);
-        var menu = SystemMenu.new();
-
-        var music = Music.new(allocator);
-
-        const clock_style_context = gtk.Widget.getStyleContext(clock.as(gtk.Widget));
-        gtk.StyleContext.addClass(clock_style_context, "clock");
-        gtk.StyleContext.addClass(clock_style_context, "clock-button");
-
-        const messages = config.message_config.messages;
-        const msg_ptr = allocator.create(std.json.Value) catch {
-            std.debug.print("Failed to allocate memory for messages\n", .{});
-            std.posix.exit(1);
-        };
-        msg_ptr.* = messages;
-        var norification = Notification.new(allocator, msg_ptr) catch {
-            std.posix.exit(1);
-        };
-
-        gtk.Box.append(left_box, menu.as(gtk.Widget));
-        gtk.Box.append(left_box, music.as(gtk.Widget));
-        gtk.Box.append(right_box, norification.as(gtk.Widget));
-        gtk.Box.append(right_box, clock.as(gtk.Widget));
-
-        gtk.Box.append(main_box, left_box.as(gtk.Widget));
-
-        var spacer = gtk.Box.new(gtk.Orientation.horizontal, 0);
-        gtk.Widget.setHexpand(spacer.as(gtk.Widget), 1);
-        gtk.Box.append(main_box, spacer.as(gtk.Widget));
-
-        gtk.Box.append(main_box, right_box.as(gtk.Widget));
+        // Build window component
+        buildUI(window, config, allocator);
 
         gtk.Widget.show(window.as(gtk.Widget));
     }
+}
+
+fn buildUI(window: *gtk.ApplicationWindow, config: *Config, allocator: std.mem.Allocator) void {
+    var main_box = gtk.Box.new(gtk.Orientation.horizontal, 1);
+    gtk.Widget.setHalign(main_box.as(gtk.Widget), gtk.Align.fill);
+    gtk.Widget.setValign(main_box.as(gtk.Widget), gtk.Align.center);
+    gtk.Window.setChild(window.as(gtk.Window), main_box.as(gtk.Widget));
+
+    var left_box = gtk.Box.new(gtk.Orientation.horizontal, 1);
+    gtk.Widget.setHalign(left_box.as(gtk.Widget), gtk.Align.start);
+    gtk.Widget.setValign(left_box.as(gtk.Widget), gtk.Align.center);
+
+    var spacer = gtk.Box.new(gtk.Orientation.horizontal, 0);
+    gtk.Widget.setHexpand(spacer.as(gtk.Widget), 1);
+
+    var right_box = gtk.Box.new(gtk.Orientation.horizontal, 1);
+    gtk.Widget.setHalign(right_box.as(gtk.Widget), gtk.Align.start);
+    gtk.Widget.setValign(right_box.as(gtk.Widget), gtk.Align.center);
+
+    // Create system menu component for controlling system and app
+    var menu = SystemMenu.new();
+    // Create music player control component
+    var music = Music.new(allocator);
+    // Create animated message component
+    const messages = config.message_config.messages;
+    const msg_ptr = allocator.create(std.json.Value) catch {
+        std.debug.print("Failed to allocate memory for messages\n", .{});
+        std.posix.exit(1);
+    };
+    msg_ptr.* = messages;
+    var norification = Notification.new(allocator, msg_ptr) catch {
+        std.posix.exit(1);
+    };
+    // Create clock component (JST)
+    var clock = Clock.new(9);
+
+    gtk.Box.append(left_box, menu.as(gtk.Widget));
+    gtk.Box.append(left_box, music.as(gtk.Widget));
+    gtk.Box.append(right_box, norification.as(gtk.Widget));
+    gtk.Box.append(right_box, clock.as(gtk.Widget));
+
+    gtk.Box.append(main_box, left_box.as(gtk.Widget));
+    gtk.Box.append(main_box, spacer.as(gtk.Widget));
+    gtk.Box.append(main_box, right_box.as(gtk.Widget));
 }
 
 pub fn main() !void {
